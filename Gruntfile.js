db4e44cbc0c01790782d96a2628a576ec9836520
Ty Underwood <ty@gaugeinteractive.com>
2014-04-06 20:53:02 -0400

    both menus working

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,9 +23,31 @@ module.exports = function(grunt) {
                 }
             }
         },
+        imagemin: {
+                dynamic: {
+                    files: [{
+                        expand: true,
+                        cwd: 'library/images/workingfiles',
+                        src: ['**/*.{png,jpg,gif}'],
+                        dest: 'library/images'
+                    }]
+                }
+        },
+        cssmin: {
+              minify: {
+                expand: true,
+                cwd: 'library/css/',
+                src: ['*.css', '!*.min.css'],
+                dest: 'library/css/',
+                ext: '.min.css'
+              }
+        }
 	});
 	grunt.loadNpmTasks('grunt-contrib-watch');
   	grunt.loadNpmTasks('grunt-contrib-compass');
   	//grunt.loadNpmTasks('grunt-contrib-uglify')
 	grunt.registerTask('default',['watch']);
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    //grunt.registerTask('imagemin', ['imagemin']);
 }
